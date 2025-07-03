177344657c7c6004c427864f1a7616322cb605e6
rupali <rupalilande3022@gmail.com>
2025-07-03 15:50:05 +0530

    Framework is developed

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class LoginPage {
+	public void loginFunctionality(String userName,String password)
+	{
+		System.out.println("user is on login page");
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println("user is on home page");
+		
+	}
+
+}
---
+++ src/test/java/testLayer/LoginTest.java
@@ -0,0 +1,13 @@
+package testLayer;
+
+import pages.LoginPage;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		LoginPage login=new LoginPage();
+		login.loginFunctionality("Admin","admin123" );
+		
+	}
+
+}
