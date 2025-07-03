7788b744359f42f0e75ffaf48e4f30451f00985d
rupali <rupalilande3022@gmail.com>
2025-07-03 16:24:39 +0530

    Home page test case added by Prajkta

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class HomePage {
+	public void validateLogo()
+	{
+		if(true)
+		{
+			System.out.println("Logo is validate");
+		}
+		else
+		{
+			System.out.println("Logo is not validate");
+		}
+	}
+
+}
---
+++ src/test/java/testLayer/HomePageTest.java
@@ -0,0 +1,15 @@
+package testLayer;
+
+import pages.HomePage;
+import pages.LoginPage;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		LoginPage login=new LoginPage();
+		login.loginFunctionality("Admin","admin123" );
+		HomePage home=new HomePage();
+		home.validateLogo();
+	}
+
+}
